3d845922eeea06fac1cb2ee7f41fe404880ad4c8
pierte <pierre.mauger@outlook.fr>
2019-02-21 15:21:46 +0100

    Ajout des tests html et javascript

---
+++ test.js
@@ -0,0 +1,5 @@
+var ensDiv = document.getElementsByTagName("div"); // donne un tableau des divs
+
+for(var i = 0; i< ensDiv.length; i++){
+    ensDiv[i].style.backgroundColor = "red"; // met tous les divs en red
+}
